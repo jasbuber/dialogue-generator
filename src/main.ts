5df3cdd064c7160e8ecaa6f0cfb7aa0a6b21262f
Tomasz Jasianek <jasbuber@gmail.com>
2018-07-14 14:06:06 +0200

    conditions and actions read from json files

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { FileService } from "./services/FileService";
+import actions from "./resources/actions.json";
+import conditions from "./resources/conditions.json";
 
 var fileService: FileService = new FileService();
 
@@ -16,5 +18,21 @@ export function loadDialogueJson(e: any) {
 window.onload = function () {
     var input = <HTMLInputElement>document.querySelector("input[type=file]");
     input.addEventListener("change", loadDialogueJson, false);
+
+    let newActionName = <HTMLSelectElement>document.getElementsByClassName("new-action-name")[0];
+    let newConditionName = <HTMLSelectElement>document.getElementsByClassName("new-condition-name")[0];
+
+    actions.values.forEach((action) => {
+        let option = <HTMLOptionElement>document.createElement("option");
+        option.text = action;
+        newActionName.add(option);
+    });
+
+    conditions.values.forEach((condition) => {
+        let option = <HTMLOptionElement>document.createElement("option");
+        option.text = condition;
+        newConditionName.add(option);
+    });
+    
 }
 
